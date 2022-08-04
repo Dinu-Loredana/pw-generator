dc596d16677af3f39193f2445a13c1818a732452
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-04 18:24:52 +0100

    add event handles to all inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw-generator';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onClickHandler = () => {
+    this.password = 'my password';
+  };
 }
